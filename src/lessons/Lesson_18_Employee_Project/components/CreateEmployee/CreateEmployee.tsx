00ae93614daa8b3f9254a2654cf20b00d7da03ff
Brauer Sergej <bmw325d@web.de>
2026-04-03 15:35:56 +0200

    add Lesson 18 Employee project

---
+++ src/lessons/Lesson_18_Employee_Project/components/CreateEmployee/CreateEmployee.tsx
@@ -0,0 +1,12 @@
+import EmployeesForm from "../EmployeesForm/EmployeesForm"
+import { PageWrapper } from "./styles"
+
+function CreateEmployee() {
+  return (
+    <PageWrapper>
+      <EmployeesForm />
+    </PageWrapper>
+  )
+}
+
+export default CreateEmployee
